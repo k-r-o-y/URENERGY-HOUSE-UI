5e4a7f652f67d99c5abf986c3ee5db446bfc326e
Andrés Möller <andres@mollertec.com>
2026-03-31 14:00:34 +0100

    Update app.js

--- app.js
+++ app.js
@@ -568,7 +568,7 @@ setInterval(() => {
 /* -------------------- INACTIVITY / DECAY -------------------- */
 
 const ALERT_MS = 3000;
-const RESET_MS = 14000;
+const RESET_MS = 8000;
 const DEPLETE = 0.025;
 
 let hasSpun = false;
